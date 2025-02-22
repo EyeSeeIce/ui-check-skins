d217e6e352edcad48ef9333726dd2bdcebe0f3aa
user <user@user.com>
2025-02-22 12:18:04 +0300

    initial

--- src/redux/services/skin.service.ts
+++ src/redux/services/skin.service.ts
@@ -4,7 +4,7 @@ import { TStep } from '../modules/route.module.ts';
 
 export const skinService = createApi({
   reducerPath: 'skin',
-  baseQuery: fetchBaseQuery({ baseUrl: 'api.realmcraft.ru/api/skin/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://api.realmcraft.ru/api/skin/' }),
   endpoints: (builder) => ({
     getSkins: builder.query<RequestSkinsDto<TStep>, null>({
       query: () => `/weapons`,
